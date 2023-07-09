776abd46450d323dd62729d06def2de41af29736
francomagliott <franco.magliotti93@gmail.com>
2023-07-09 17:20:49 -0300

    se modifico productos y se agrego nueva seccion de sorteos

---
+++ sorteo/sorteo.js
@@ -0,0 +1,119 @@
+const wheel = document.getElementById("wheel");
+const spinBtn = document.getElementById("spin-btn");
+const finalValue = document.getElementById("final-value");
+//Object that stores values of minimum and maximum angle for a value
+const rotationValues = [
+  { minDegree: 0, maxDegree: 30, value: "Java" },
+  { minDegree: 31, maxDegree: 90, value: "Javascript" },
+  { minDegree: 91, maxDegree: 150, value: "Kotlin" },
+  { minDegree: 151, maxDegree: 210, value: "C++" },
+  { minDegree: 211, maxDegree: 270, value: "C#" },
+  { minDegree: 271, maxDegree: 330, value: "Python" },
+  { minDegree: 331, maxDegree: 360, value: "Java" },
+];
+//Size of each piece
+const data = [16, 16, 16, 16, 16, 16];
+//background color for each piece
+let pieColors = [
+  "#FF0000",
+  "#FF6A6A",
+  "#FF0000",
+  "#FF6A6A",
+  "#FF0000",
+  "#FF6A6A",
+];
+
+
+//Create chart
+let myChart = new Chart(wheel, {
+  //Plugin for displaying text on pie chart
+  plugins: [ChartDataLabels],
+  //Chart Type Pie
+  type: "pie",
+  data: {
+    //Labels(values which are to be displayed on chart)
+    labels: ["Javascript", "Java", "Python", "C#", "C++", "Kotlin"],
+    //Settings for dataset/pie
+    datasets: [
+      {
+        backgroundColor: pieColors,
+        data: data,
+      },
+    ],
+  },
+  options: {
+    //Responsive chart
+    responsive: true,
+    animation: { duration: 0 },
+    plugins: {
+      //hide tooltip and legend
+      tooltip: false,
+      legend: {
+        display: false,
+      },
+      //display labels inside pie chart
+      datalabels: {
+        color: "#ffffff",
+        formatter: (_, context) => context.chart.data.labels[context.dataIndex],
+        font: { size: 24 },
+      },
+    },
+  },
+});
+//display value based on the randomAngle
+const valueGenerator = (angleValue) => {
+  for (let i of rotationValues) {
+    //if the angleValue is between min and max then display it
+    if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
+      finalValue.innerHTML = `<p>Value: ${i.value}</p>`;
+      spinBtn.disabled = false;
+      break;
+    }
+  }
+};
+//Spinner count
+let count = 0;
+//100 rotations for animation and last rotation for result
+let resultValue = 101;
+//Start spinning
+spinBtn.addEventListener("click", () => {
+  spinBtn.disabled = true;
+  //Empty final value
+  finalValue.innerHTML = `<p>Good Luck!</p>`;
+  //Generate random degrees to stop at
+  let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
+  //Interval for rotation animation
+  let rotationInterval = window.setInterval(() => {
+    //Set rotation for piechart
+    /*
+    Initially to make the piechart rotate faster we set resultValue to 101 so it rotates 101 degrees at a time and this reduces by 1 with every count. Eventually on last rotation we rotate by 1 degree at a time.
+    */
+    myChart.options.rotation = myChart.options.rotation + resultValue;
+    //Update chart with new value;
+    myChart.update();
+    //If rotation>360 reset it back to 0
+    if (myChart.options.rotation >= 360) {
+      count += 1;
+      resultValue -= 5;
+      myChart.options.rotation = 0;
+    } else if (count > 15 && myChart.options.rotation == randomDegree) {
+      valueGenerator(randomDegree);
+      clearInterval(rotationInterval);
+      count = 0;
+      resultValue = 101;
+    }
+  }, 10);
+});
+
+const burger = document.getElementById("menu-button");
+const navbar = document.getElementById("navbar");
+const closeMenu = document.getElementById("close-menu");
+
+burger.addEventListener("click", () => {
+    navbar.style.transform = "translateX(0%)"
+    console.log("a")
+})
+
+closeMenu.addEventListener("click", () => {
+    navbar.style.transform = "translateX(-100%)"
+})
